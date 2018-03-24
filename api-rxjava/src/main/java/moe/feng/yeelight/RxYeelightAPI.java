07af9cbf544c341ae9a7189cf410231b2cd1203c
Fung Gwo <fython@163.com>
2018-03-24 20:04:25 +0800

    Down to Java 7 compatibility
    Signed-off-by: Fung Gwo <fython@163.com>

--- api-rxjava/src/main/java/moe/feng/yeelight/RxYeelightAPI.java
+++ api-rxjava/src/main/java/moe/feng/yeelight/RxYeelightAPI.java
@@ -1,8 +1,8 @@
 package moe.feng.yeelight;
 
-import io.reactivex.BackpressureStrategy;
-import io.reactivex.Flowable;
-import io.reactivex.Single;
+import io.reactivex.*;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 import moe.feng.yeelight.model.Bulb;
 import moe.feng.yeelight.model.Method;
 
@@ -23,55 +23,66 @@ public final class RxYeelightAPI {
         return discoverBulbs(10000);
     }
 
-    public static Flowable<Bulb> discoverBulbs(int timeout) {
-        return Flowable.create(emitter -> {
-            DatagramSocket socket = new DatagramSocket();
-            socket.setSoTimeout(2000);
+    public static Flowable<Bulb> discoverBulbs(final int timeout) {
+        return Flowable.create(new FlowableOnSubscribe<Bulb>() {
+            @Override
+            public void subscribe(FlowableEmitter<Bulb> emitter) throws Exception {
+                DatagramSocket socket = new DatagramSocket();
+                socket.setSoTimeout(2000);
 
-            byte[] data = YeelightAPI.DATA_DISCOVER.getBytes();
+                byte[] data = YeelightAPI.DATA_DISCOVER.getBytes();
 
-            DatagramPacket packet = new DatagramPacket(
-                    data, data.length, Inet4Address.getByName("239.255.255.250"), 1982);
-            socket.send(packet);
+                DatagramPacket packet = new DatagramPacket(
+                        data, data.length, Inet4Address.getByName("239.255.255.250"), 1982);
+                socket.send(packet);
 
-            packet.setData(new byte[65507]);
-            packet.setLength(65507);
+                packet.setData(new byte[65507]);
+                packet.setLength(65507);
 
-            long startTime = System.currentTimeMillis();
+                long startTime = System.currentTimeMillis();
 
-            while (true) {
-                try {
-                    socket.receive(packet);
+                while (true) {
+                    try {
+                        socket.receive(packet);
 
-                    String text = new String(packet.getData());
-                    Map<String, String> map = new HashMap<>();
-                    for (String line : text.split("\n")) {
-                        if (line.contains(":")) {
-                            map.put(line.substring(0, line.indexOf(":")).trim(),
-                                    line.substring(line.indexOf(":") + 1).trim());
+                        String text = new String(packet.getData());
+                        Map<String, String> map = new HashMap<>();
+                        for (String line : text.split("\n")) {
+                            if (line.contains(":")) {
+                                map.put(line.substring(0, line.indexOf(":")).trim(),
+                                        line.substring(line.indexOf(":") + 1).trim());
+                            }
                         }
+
+                        Bulb bulb = Bulb.parseRawData(map);
+                        emitter.onNext(bulb);
+                    } catch (SocketTimeoutException ignored) {
+
                     }
-
-                    Bulb bulb = Bulb.parseRawData(map);
-                    emitter.onNext(bulb);
-                } catch (SocketTimeoutException ignored) {
-
+                    if (timeout > 0 && System.currentTimeMillis() - startTime >= timeout) {
+                        break;
+                    }
                 }
-                if (timeout > 0 && System.currentTimeMillis() - startTime >= timeout) {
-                    break;
-                }
+
+                emitter.onComplete();
             }
-
-            emitter.onComplete();
         }, BackpressureStrategy.BUFFER);
     }
 
     public static Flowable<Bulb> onNewBulb() {
         return discoverBulbs(-1)
-                .filter(bulb -> !sBulbs.contains(bulb))
-                .map(bulb -> {
-                    sBulbs.add(bulb);
-                    return bulb;
+                .filter(new Predicate<Bulb>() {
+                    @Override
+                    public boolean test(Bulb bulb) throws Exception {
+                        return !sBulbs.contains(bulb);
+                    }
+                })
+                .map(new Function<Bulb, Bulb>() {
+                    @Override
+                    public Bulb apply(Bulb bulb) throws Exception {
+                        sBulbs.add(bulb);
+                        return bulb;
+                    }
                 });
     }
 
@@ -79,12 +90,15 @@ public final class RxYeelightAPI {
         sBulbs.clear();
     }
 
-    public static Single<Method.Response> call(Method method) {
-        return Single.create(emitter -> {
-            try {
-                emitter.onSuccess(method.call());
-            } catch (Exception e) {
-                emitter.onError(e);
+    public static Single<Method.Response> call(final Method method) {
+        return Single.create(new SingleOnSubscribe<Method.Response>() {
+            @Override
+            public void subscribe(SingleEmitter<Method.Response> emitter) throws Exception {
+                try {
+                    emitter.onSuccess(method.call());
+                } catch (Exception e) {
+                    emitter.onError(e);
+                }
             }
         });
     }
